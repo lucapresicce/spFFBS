8b27a69109ac83e694dac5e1199bf8cb54fa7040
lucapresicce <l.presicce@campus.unimib.it>
2025-01-30 19:44:49 +0100

    initial commit

---
+++ R/spFFBS-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib spFFBS, .registration = TRUE
+## usethis namespace: end
+NULL
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
