6e4a68f7f2683efd7d2aa7aae51affa13b0cb281
lucapresicce <l.presicce@campus.unimib.it>
2025-11-24 13:22:04 +0100

    Set up pkgdown Github pages

---
+++ src/FFBS-VAR.cpp
@@ -0,0 +1,8 @@
+// #include <RcppArmadillo.h>
+// #include "FFBS-DYNBPS.h"
+// #include "FFBS-VAR.h"
+//
+// // [[Rcpp::depends(RcppArmadillo)]]
+//
+// using namespace Rcpp;
+// using namespace arma;
